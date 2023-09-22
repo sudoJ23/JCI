efec1f9b3539e45be04507ecd2e5145fdf204775
sudoj23 <herdiantojeremi@gmail.com>
2023-09-23 03:41:17 +0700

    move product and master to auth group

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/master', function() {
-    return view('layouts.master');
-});
 
-Route::resource('product', ProductController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('product', ProductController::class);
+    Route::get('/master', function() {
+        return view('layouts.master');
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
