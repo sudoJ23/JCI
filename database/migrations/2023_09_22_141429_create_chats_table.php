5caf5ee5950798ae089af3f99c520ac28f47b406
Anvy123 <bintangramadhan4974@gmail.com>
2023-09-22 21:21:36 +0700

    make model & migration database

---
+++ database/migrations/2023_09_22_141429_create_chats_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->text('message');
+            $table->integer('sender_id')->references('id')->on('users');
+            $table->integer('receiver_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
