39d75124fd4c675591d337399a6db8defac201fb
iniryan <riyanadi552@gmail.com>
2023-09-23 23:54:44 +0700

    contact n about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
 
-<div class="container py-5">
-    <div class="row g-5">
-        <div class="col-12">
-            <h1 class="text-white text-center">Tentang Kami</h1>
-        </div>
+<section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-12 col-md-8 mx-auto">
+        <h1 class="fw-light text-white fw-bold">Tentang Kami</h1>
+        <p class="lead text-white mt-5">
+            SITIK, Sistem Informasi Distribusi Logistik adalah solusi revolusioner bagi para petani di Desa Wonoanti, adalah jembatan baru yang menghubungkan Anda langsung dengan UMKM dan produsen tanpa perlu melewati tengkulak. Kami hadir untuk menghadirkan perubahan besar dalam cara Anda menjual produk pertanian Anda. SITIK adalah teknologi yang dikembangkan oleh KIM "Nganggo Roso" yang bekerja sama dengan 5 Mahasiswa Institut Teknologi Telkom Surabaya dalam sebuah kompetisi yang disebut hackathon untuk mengembangkan sebuah ide bisnis untuk menyelesaikan masalah petani di Desa Wonoanti. 
+        </p>
+      </div>
     </div>
-</div>
+  </section>
 
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 
-<div class="container py-5">
-    <div class="row g-5">
-        <div class="col-12">
-            <h1 class="text-white text-center">Kontak Kami</h1>
-        </div>
+<section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-12 col-md-8 mx-auto">
+        <h1 class="fw-light text-white fw-bold">Kontak Kami</h1>
+        <a class="lead text-white" href="https://nganggo-roso.kim.id/">https://nganggo-roso.kim.id/ </a> <br>
+        <a href="https://wa.me/6285159836911" class="lead text-white">08123456789</p>
+      </div>
     </div>
-</div>
+  </section>
 
 @endsection
