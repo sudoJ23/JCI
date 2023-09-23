3fd7be5cee0ca10ca5ac295e77b2e00cc68647c1
Anvy123 <bintangramadhan4974@gmail.com>
2023-09-24 05:24:59 +0700

    ..

--- resources/views/company/product.blade.php
+++ resources/views/company/product.blade.php
@@ -6,7 +6,6 @@
 @section('content')
     @foreach ($products as $product)
         <div class="card" style="width: 18rem;">
-            {{-- <img src="..." class="card-img-top" alt="..."> --}}
             <div class="card-body">
                 <h5 class="card-title">{{ $product->name }}</h5>
                 <p class="card-text">{{ $product->description }}</p>
