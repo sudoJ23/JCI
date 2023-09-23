7efa5c82745fd2b04c8757f70d6f0298a5d7bf72
Anvy123 <bintangramadhan4974@gmail.com>
2023-09-23 09:37:28 +0700

    update migration

--- database/migrations/2023_09_22_134618_create_products_table.php
+++ database/migrations/2023_09_22_134618_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained();
             $table->string('name');
             $table->string('price');
             $table->string('qty_init');
