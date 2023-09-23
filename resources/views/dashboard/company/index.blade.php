d6b626cf843957465baaa1494536bd8fde29c6a8
sudoj23 <herdiantojeremi@gmail.com>
2023-09-23 22:09:28 +0700

    ~

---
+++ resources/views/dashboard/company/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('title')
+    Dashboard company
+@endsection
+
+@section('content')
+    ini dashboard company
+@endsection
---
+++ resources/views/dashboard/farmer/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('title')
+    Dashboard farmer
+@endsection
+
+@section('content')
+    ini dashboard farmer
+@endsection
