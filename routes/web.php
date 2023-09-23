72de1dc26b537e5d652cc3dc3ab10cf97ec675d9
sudoj23 <herdiantojeremi@gmail.com>
2023-09-24 05:24:53 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DeliveryBumdesController;
 use App\Http\Controllers\FarmerDetailController;
@@ -32,9 +33,7 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/chat', function() {
-    return view('chat.index');
-});
+Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
