ff3bd63cbde8e4361c64247a613b13bb7b378545
Anvy123 <bintangramadhan4974@gmail.com>
2023-09-23 14:04:50 +0700

    adding create delivery bumdes

--- app/Models/Delivery_bumdes.php
+++ app/Models/Delivery_bumdes.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Delivery_bumdes extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeliveryBumdesController;
 use App\Http\Controllers\FarmerDetailController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
@@ -29,9 +30,12 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::resource('product', ProductController::class);
     Route::resource('farmer_detail', FarmerDetailController::class);
+    Route::resource('delivery_bumdes', DeliveryBumdesController::class);
+
     Route::get('/master', function() {
         return view('layouts.master');
     });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
